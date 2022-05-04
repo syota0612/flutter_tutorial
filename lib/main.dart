851e1015e82ca80c05e92ad65b314e6d61091b99
syota0612 <syota.19910612bscplog@gmail.com>
2022-05-04 17:22:51 +0900

    ランダムな英単語を作成するクラスをWidgetのフォルダに切り出し

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+
+import './widgets/RandomWord.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key); // 初期ビルド時のみ
   @override
-  Widget build(BuildContext context) { // ホットリロード時に再ビルド
-    final wordPair = WordPair.random();
+  Widget build(BuildContext context) {
+    // ホットリロード時に再ビルド
     return MaterialApp(
         title: 'Welcome to Flutter',
         home: Scaffold(
             appBar: AppBar(
               title: const Text('Welcome to Flutter'),
             ),
-            body: Center(
-              child: Text(wordPair.asPascalCase),
+            body: const Center(
+              child: RandomWords(),
             )));
   }
 }
