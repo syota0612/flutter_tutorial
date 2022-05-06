c6b25c3669d8bdc1529be4918712c5aec540e7e5
syota0612 <syota.19910612bscplog@gmail.com>
2022-05-06 20:58:05 +0900

    簡易メニューを作成

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,49 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const data = [
+      Text("item0"),
+      Text("item1"),
+      Text("item2"),
+      Text("item3"),
+      Text("item4"),
+    ];
     // ホットリロード時に再ビルド
     return MaterialApp(
-      title: 'Direction',
-      home: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          verticalDirection: VerticalDirection.up,
-          children: const <Widget>[
-            Text(
-              'Direction',
-              style: TextStyle(color: Colors.blue, fontSize: 30),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('ListView'),
             ),
-            Text(
-              'Direction',
-              style: TextStyle(color: Colors.red, fontSize: 20)
-            ),
-          ],
-        ),
-      ),
-    );
+            body: ListView(
+              children: [
+                _menuitem('メニュー1', Icon(Icons.settings)),
+                _menuitem('メニュー2', Icon(Icons.map)),
+                _menuitem('メニュー3', Icon(Icons.room)),
+                _menuitem('メニュー4', Icon(Icons.local_shipping)),
+                _menuitem('メニュー5', Icon(Icons.airplanemode_active)),
+              ],
+            )));
+  }
+
+  Widget _menuitem(String title, Icon icon) {
+    return GestureDetector(
+        child: Container(
+            padding: EdgeInsets.all(8.0),
+            decoration: const BoxDecoration(
+                border:
+                    Border(bottom: BorderSide(width: 1.0, color: Colors.grey))),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.all(10.0),
+                  child: icon,
+                ),
+                Text(title,
+                    style: const TextStyle(color: Colors.black, fontSize: 18.0))
+              ],
+            )),
+        onTap: () {
+          print('onTap calld');
+        });
   }
 }
