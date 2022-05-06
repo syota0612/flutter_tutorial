4fe6553f3ec8e584fce20da2362eda93e765e94c
syota0612 <syota.19910612bscplog@gmail.com>
2022-05-06 21:06:28 +0900

    ListTileの操作

--- lib/main.dart
+++ lib/main.dart
@@ -36,24 +36,25 @@ class MyApp extends StatelessWidget {
   }
 
   Widget _menuitem(String title, Icon icon) {
-    return GestureDetector(
-        child: Container(
-            padding: EdgeInsets.all(8.0),
-            decoration: const BoxDecoration(
-                border:
-                    Border(bottom: BorderSide(width: 1.0, color: Colors.grey))),
-            child: Row(
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.all(10.0),
-                  child: icon,
-                ),
-                Text(title,
-                    style: const TextStyle(color: Colors.black, fontSize: 18.0))
-              ],
-            )),
+    return Container(
+      decoration: const BoxDecoration(
+          border: Border(bottom: BorderSide(width: 1.0, color: Colors.grey))),
+      child: ListTile(
+        leading: icon,
+        title: Text(
+          title,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 18.0,
+          ),
+        ),
         onTap: () {
-          print('onTap calld');
-        });
+          print('onTap calld.');
+        },
+        onLongPress: () {
+          print('onLongPress calld.');
+        },
+      ),
+    );
   }
 }
