f169fcfbb86c47c497e8cdeab5ae875a758f7783
syota0612 <syota.19910612bscplog@gmail.com>
2022-05-08 22:13:06 +0900

    Cardの操作

--- lib/main.dart
+++ lib/main.dart
@@ -17,40 +17,17 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Stack'),
         ),
-        body: Stack(
-          alignment: Alignment.bottomRight,
-          children: <Widget>[
-            SizedBox(
-              width: 400.0,
-              height: 400.0,
-              child: Container(color: Colors.orange,)
-            ),
-            Positioned(
-              left: 20.0,
-              top: 20.0,
-              width: 300.0,
-              height: 300.0,
-              child: Container(color: Colors.blue,),
-            ),
-            Positioned(
-              left: 10.0,
-              top: 10.0,
-              width: 100.0,
-              height: 100.0,
-              child: Container(color: Colors.green,),
-            ),
-            Positioned(
-              left: 120.0,
-              top: 120.0,
-              width: 100.0,
-              height: 100.0,
-              child: Container(color: Colors.orange,),
-            ),
-            const Text('Test'),
-            // const Center(
-            //   child: Text('Test'),
-            // ),
-          ],
+        body: Card(
+          margin: const EdgeInsets.all(50.0),
+          child: Container(
+            margin: const EdgeInsets.all(10.0),
+            width: 300,
+            height: 100,
+            child: const Text(
+              'Card',
+              style: TextStyle(fontSize: 30),
+            )
+          )
         )
       )
     );
