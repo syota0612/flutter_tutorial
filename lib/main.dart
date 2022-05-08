ee34f2d4f541436f17cb7e9ccb3f725917de7826
syota0612 <syota.19910612bscplog@gmail.com>
2022-05-08 20:30:56 +0900

    ウィジェットを重ねる

--- lib/main.dart
+++ lib/main.dart
@@ -10,50 +10,28 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key); // 初期ビルド時のみ
 
   @override
+  // ホットリロード時に再ビルド
   Widget build(BuildContext context) {
-    final grid = [
-      "pic0",
-      "pic1",
-      "pic2",
-      "pic3",
-      "pic4",
-      "pic5",
-    ];
-    // final list = [
-    //   _photoItem("pic0"),
-    //   _photoItem("pic1"),
-    //   _photoItem("pic2"),
-    //   _photoItem("pic3"),
-    //   _photoItem("pic4"),
-    //   _photoItem("pic5"),
-    // ];
-    // ホットリロード時に再ビルド
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('GridView'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Stack'),
+        ),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
             ),
-            body: GridView.builder(
-              scrollDirection: Axis.horizontal,
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 150,
-              ),
-              itemBuilder: (BuildContext context, int index) {
-                if (index >= grid.length) {
-                  grid.addAll(["pic0", "pic1", "pic2", "pic3", "pic4", "pic5",]);
-                }
-                return _photoItem(grid[index]);
-              },
-            )));
-  }
-
-  Widget _photoItem(String image) {
-    final assetsImage = "./assets/img/" + image + ".png";
-    return Container(
-      child: Image.asset(
-        assetsImage,
-        fit: BoxFit.cover,
-      ),
+            Container(
+              width: 50,
+              height: 80,
+              color: Colors.orange,
+            )
+          ],
+        )
+      )
     );
   }
 }
